96d12d52a8d37b6fa00bdaa94333cc4c7d459dab
AlfatUber <167688170+AlfatUber@users.noreply.github.com>
2025-05-11 14:34:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -230,9 +230,11 @@ async def verify_challenge(username: str = Form(...), code: str = Form(...)):
     try:
         if hasattr(cl, "challenge_resolve_security_code"):
             cl.challenge_resolve_security_code(code)
-        else:
+        elif:
             cl.challenge_code_handler = lambda u, c: code
             cl.challenge_resolve()
+        else:
+            cl.login(username, password, verification_code=code)
 
         await save_client_session(cl, username)
 
